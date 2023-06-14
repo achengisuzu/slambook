f88682cf80ecb2435a005c673aaa3feb3d695865
Angel Cheng <angelhyc@umich.edu>
2023-06-14 15:41:12 -0400

    ch5 practice undistort Image

---
+++ ch5/imageBasics/build/CMakeFiles/undistort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/undistort.dir/undistortImage.cpp.o"
+  "CMakeFiles/undistort.dir/undistortImage.cpp.o.d"
+  "undistort"
+  "undistort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/undistort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
