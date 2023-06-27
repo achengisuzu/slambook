06d06856e0f99a7580bad9a988cea61ddff9e75f
Angel Cheng <angelhyc@umich.edu>
2023-06-27 13:38:18 -0400

    moved directoies of guassNewton

---
+++ ch6/guassNewton/build/CMakeFiles/guassNewton.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/guassNewton.dir/guassNewton.cpp.o"
+  "CMakeFiles/guassNewton.dir/guassNewton.cpp.o.d"
+  "guassNewton"
+  "guassNewton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/guassNewton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
