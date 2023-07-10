fa8557a63c15a2d4ba7a7ea0949268ff4a258fb8
Angel Cheng <angelhyc@umich.edu>
2023-07-10 14:33:05 -0400

    3d2d error estimation

---
+++ ch7/pnp/build/CMakeFiles/pose.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pose.dir/pose_estimation_3d2d.cpp.o"
+  "CMakeFiles/pose.dir/pose_estimation_3d2d.cpp.o.d"
+  "pose"
+  "pose.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
