56269129b98cf103b66a7c048149bd91036dc7ce
Angel Cheng <angelhyc@umich.edu>
2023-07-14 15:01:10 -0400

    optical flow

---
+++ ch8/opticalflow/build/CMakeFiles/opticalflow.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opticalflow.dir/opticalflow.cpp.o"
+  "CMakeFiles/opticalflow.dir/opticalflow.cpp.o.d"
+  "opticalflow"
+  "opticalflow.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opticalflow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
