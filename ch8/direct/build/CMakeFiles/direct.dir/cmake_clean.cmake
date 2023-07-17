1ebc10b66a6000662ced05746773825bc59bcadf
Angel Cheng <angelhyc@umich.edu>
2023-07-17 14:33:20 -0400

    direct method

---
+++ ch8/direct/build/CMakeFiles/direct.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/direct.dir/direct_method.cpp.o"
+  "CMakeFiles/direct.dir/direct_method.cpp.o.d"
+  "direct"
+  "direct.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/direct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
