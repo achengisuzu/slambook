59776b9e8dce0f7c2190656f38486258e864642c
Angel Cheng <angelhyc@umich.edu>
2023-07-24 15:12:28 -0400

    move stuff around

---
+++ ch9/build/CMakeFiles/bag.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bag.dir/bundleadjustmentg2o.cpp.o"
+  "CMakeFiles/bag.dir/bundleadjustmentg2o.cpp.o.d"
+  "bag"
+  "bag.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bag.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch9/build/CMakeFiles/bag.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for bag.
+# This may be replaced when dependencies are built.
