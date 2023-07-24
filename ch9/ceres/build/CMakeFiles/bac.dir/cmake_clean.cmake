33f37029538c8355b4d34dc62980746d91e7db88
Angel Cheng <angelhyc@umich.edu>
2023-07-24 14:28:58 -0400

    bundleadjustment ceres

---
+++ ch9/ceres/build/CMakeFiles/bac.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bac.dir/bundleadjustmentceres.cpp.o"
+  "CMakeFiles/bac.dir/bundleadjustmentceres.cpp.o.d"
+  "bac"
+  "bac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
