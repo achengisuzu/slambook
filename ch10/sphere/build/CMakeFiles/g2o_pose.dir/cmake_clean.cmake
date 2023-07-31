c28689c0d0625961af5c366259f30bfbf89acb19
Angel Cheng <angelhyc@umich.edu>
2023-07-31 14:22:08 -0400

    posegraph with g2o

---
+++ ch10/sphere/build/CMakeFiles/g2o_pose.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/g2o_pose.dir/pose_graph_g2o_SE3.cpp.o"
+  "CMakeFiles/g2o_pose.dir/pose_graph_g2o_SE3.cpp.o.d"
+  "g2o_pose"
+  "g2o_pose.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/g2o_pose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch10/sphere/build/CMakeFiles/g2o_pose.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for g2o_pose.
+# This may be replaced when dependencies are built.
