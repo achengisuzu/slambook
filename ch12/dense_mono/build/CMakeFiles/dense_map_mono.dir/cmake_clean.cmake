aebd074c53f870bc0bb184dc6329a861c699ebc0
Angel Cheng <angelhyc@umich.edu>
2023-08-11 15:59:27 -0400

    ch11 code of mapping

---
+++ ch12/dense_mono/build/CMakeFiles/dense_map_mono.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dense_map_mono.dir/dense_mapping.cpp.o"
+  "CMakeFiles/dense_map_mono.dir/dense_mapping.cpp.o.d"
+  "dense_map_mono"
+  "dense_map_mono.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dense_map_mono.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch12/dense_mono/build/CMakeFiles/dense_map_mono.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dense_map_mono.
+# This may be replaced when dependencies are built.
